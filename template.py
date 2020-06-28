32e6cba6dc114e4186d40925be040c5514b722ab
Gtavexir <gtavexir@kookmin.ac.kr>
2020-06-28 11:30:18 +0900

    issue #4 현재까지 작성된 과제 결과물 Git에 추가

---
+++ template.py
@@ -0,0 +1,13 @@
+html = b"""
+<html>
+    <body>
+        <form method = "get" action = "">
+                A = <input type = "number" name = "a"><br><br>
+                B = <input type = "number" name = "b"><br><br>
+                <input type = "submit">
+        </form>
+            Sum = %(sum)s</br>
+            Mul = %(mul)s</br>
+    </body>
+</html>
+"""
